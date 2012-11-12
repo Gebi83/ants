7d30d7c3a5777a865ccd6317eca13425ac12132b
Gebi <dampf.hans13@googlemail.com>
2012-11-12 12:43:02 +0100

    more logic for ants; logging implemented but not working

--- Ants/src/Ant.java
+++ Ants/src/Ant.java
@@ -1,6 +1,52 @@
 
+/**
+ * represents one ant with its position, next step (next position) and its destination.
+ * @author Gebi
+ *
+ */
 public class Ant {
-	
+	/**
+	 * current position
+	 */
 	private Tile position;
+	
+	/**
+	 * next step
+	 */
+	private Tile nextPosition;
+	
+	/**
+	 * destination
+	 */
+	private Tile destination;
 
+	public Ant(Tile position) {
+		this.position = position;
+	}
+
+	public Tile getDestination() {
+		return destination;
+	}
+
+	public void setDestination(Tile destination) {
+		this.destination = destination;
+	}
+
+	public Tile getPosition() {
+		return position;
+	}
+
+	public void setPosition(Tile position) {
+		this.position = position;
+	}
+
+	public Tile getNextPosition() {
+		return nextPosition;
+	}
+
+	public void setNextPosition(Tile nextPosition) {
+		this.nextPosition = nextPosition;
+	}
+
+	
 }
--- Ants/src/GebiBot.java
+++ Ants/src/GebiBot.java
@@ -1,5 +1,6 @@
 
 import java.io.IOException;
+import java.io.ObjectInputStream.GetField;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
@@ -10,15 +11,25 @@ import java.util.Map;
 import java.util.Set;
 import java.util.TreeSet;
 
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+
 /**
  * Starter bot implementation.
  */
 public class GebiBot extends Bot {
+	
+	private static Logger logger;
 
 	/**
 	 * all new locations for my ants for one turn.
 	 */
 	private Map<Tile, Tile> orders = new HashMap<Tile, Tile>();
+	
+	/**
+	 * list with positions where ants should not go (own hills, walls, ...)
+	 */
+	private List<Tile> forbiddenPositions = new ArrayList<Tile>();
 
 	/**
 	 * locations that haven´t been "seen".
@@ -37,7 +48,12 @@ public class GebiBot extends Bot {
 	 * @throws IOException if an I/O error occurs
 	 */
 	public static void main(String[] args) throws IOException {
+		
+		PropertyConfigurator.configure(".\\src\\log4j.properties");
 		new GebiBot().readSystemInput();
+		
+		logger = Logger.getLogger(GebiBot.class.getCanonicalName());
+		logger.error("Test");
 	}
 
 	/**
@@ -50,16 +66,15 @@ public class GebiBot extends Bot {
 		Ants ants = getAnts();
 		
 		createAntList(ants);
+		letAntsMove();
 		
 		orders.clear();
 
 		// prevent stepping on own hill
 		for (Tile myHill : ants.getMyHills()) {
-			orders.put(myHill, null);
+			orders.put(myHill, null);//TODO remove old version
+			forbiddenPositions.add(myHill);
 		}
-
-
-		Map<Tile, Tile> foodTargets = new HashMap<Tile, Tile>();
 
 		// add all locations to unseen tiles set, run once
 		if (unseenTiles == null) {
@@ -77,24 +92,41 @@ public class GebiBot extends Bot {
 				locIter.remove();
 			}
 		}
+		
+		
 
 		// find close food
 		List<Route> foodRoutes = new ArrayList<Route>();
 		TreeSet<Tile> sortedFood = new TreeSet<Tile>(ants.getFoodTiles());
 		TreeSet<Tile> sortedAnts = new TreeSet<Tile>(ants.getMyAnts());
 		for (Tile foodLoc : sortedFood) {
-			for (Tile antLoc : sortedAnts) {
-				int distance = ants.getDistance(antLoc, foodLoc);
-				Route route = new Route(antLoc, foodLoc, distance);
-				foodRoutes.add(route);
+//			for (Tile antLoc : sortedAnts) {
+//				int distance = ants.getDistance(antLoc, foodLoc);
+//				Route route = new Route(antLoc, foodLoc, distance);
+//				foodRoutes.add(route);
+//			}
+			
+			List<Ant> antsWithNoDestination = getAntsWithoutDestination();
+			for (Iterator<Ant> it = antsWithNoDestination.iterator(); it.hasNext();) {
+				Ant ant = it.next();
+					Tile antLoc = ant.getPosition();
+					int distance = ants.getDistance(antLoc, foodLoc);
+					Route route = new Route(antLoc, foodLoc, distance);
+					foodRoutes.add(route);
+				
+				
 			}
 		}
 		Collections.sort(foodRoutes);
+		
+		Map<Tile, Ant> foodTargets = new HashMap<Tile, Ant>();
 		for (Route route : foodRoutes) {
 			if (!foodTargets.containsKey(route.getEnd())
 					&& !foodTargets.containsValue(route.getStart())
 					&& doMoveLocation(route.getStart(), route.getEnd())) {
-				foodTargets.put(route.getEnd(), route.getStart());
+				
+				getAntByLocation(route.getStart()).setDestination(route.getEnd());
+				foodTargets.put(route.getEnd(), getAntByLocation(route.getStart()));
 			}
 		}
 		
@@ -107,37 +139,45 @@ public class GebiBot extends Bot {
         // attack hills
         List<Route> hillRoutes = new ArrayList<Route>();
         for (Tile hillLoc : enemyHills) {
-            for (Tile antLoc : sortedAnts) {
-                if (!orders.containsValue(antLoc)) {
-                    int distance = ants.getDistance(antLoc, hillLoc);
-                    Route route = new Route(antLoc, hillLoc, distance);
+        	
+        	List<Ant> antsWithNoDestination = getAntsWithoutDestination();
+			for (Iterator<Ant> it = antsWithNoDestination.iterator(); it.hasNext();) {
+				Ant ant = it.next();
+				
+                
+                    int distance = ants.getDistance(ant.getPosition(), hillLoc);
+                    Route route = new Route(ant.getPosition(), hillLoc, distance);
                     hillRoutes.add(route);
-                }
+                
             }
         }
         Collections.sort(hillRoutes);
         for (Route route : hillRoutes) {
-            doMoveLocation(route.getStart(), route.getEnd());
+        	if (doMoveLocation(route.getStart(), route.getEnd())) {
+        		getAntByLocation(route.getStart()).setDestination(route.getEnd());
+        	}
         }
 
 		// explore unseen areas
-		for (Tile antLoc : sortedAnts) {
-			if (!orders.containsValue(antLoc)) {
+        List<Ant> antsWithNoDestination = getAntsWithoutDestination();
+		for (Iterator<Ant> it = antsWithNoDestination.iterator(); it.hasNext();) {
+			Ant ant = it.next();
 				List<Route> unseenRoutes = new ArrayList<Route>();
 //TODO better technique here. at the moment all distances to all unseen locations are calculated
+				
 				for (Tile unseenLoc : unseenTiles) {
-					int distance = ants.getDistance(antLoc, unseenLoc);
-					Route route = new Route(antLoc, unseenLoc, distance);
+					int distance = ants.getDistance(ant.getPosition(), unseenLoc);
+					Route route = new Route(ant.getPosition(), unseenLoc, distance);
 					unseenRoutes.add(route);
 				}
 				Collections.sort(unseenRoutes);
 				for (Route route : unseenRoutes) {
 					if (doMoveLocation(route.getStart(), route.getEnd())) {
-						break;
+						getAntByLocation(route.getStart()).setDestination(route.getEnd());
 					}
 				}
-			}
 		}
+		
 
 
 		// unblock hills
@@ -153,6 +193,39 @@ public class GebiBot extends Bot {
 	}
 
 	/**
+	 * moves all ants with destination one step. If moving to destination is not possible, 
+	 * remove destination.
+	 */
+	private void letAntsMove() {
+		
+		for (Iterator<Ant> it = myAntList.iterator(); it.hasNext(); ) {
+			Ant ant = it.next();
+			if (ant.getDestination() != null){
+				
+				if (!doMoveLocation(ant.getPosition(), ant.getDestination())) {
+					ant.setDestination(null);
+				}
+			}
+					
+		}
+		
+	}
+
+	private List<Ant> getAntsWithoutDestination() {
+		
+		List<Ant> antsWithoutDestination = new ArrayList<Ant>();
+		
+		for (Iterator<Ant> it = myAntList.iterator(); it.hasNext();) {
+			Ant ant = it.next();
+			if (ant.getDestination() == null) {
+				antsWithoutDestination.add(ant);
+			}
+		}
+		
+		return antsWithoutDestination;
+	}
+
+	/**
 	 * creates or modifies the list with all my ants.
 	 * checks if new ants are alive.
 	 * checks if old ants are still alive.
@@ -161,21 +234,67 @@ public class GebiBot extends Bot {
 	 */
 	private void createAntList(Ants ants) {
 		
-		List<Tile> oldAnts = getAntPositions();
-		
+//		List<Tile> antDestinations = getAntDestinations();
 		TreeSet<Tile> sortedAnts = new TreeSet<Tile>(ants.getMyAnts());
+	
+		for (Iterator<Ant> it = myAntList.iterator();it.hasNext();) {
+			
+			
+			Ant ant = it.next();
+			
+			//ant did not move in last turn
+			if (ant.getNextPosition() == null
+					&& !sortedAnts.contains(ant.getPosition())) {
+				it.remove();//ant is dead (no ant at its destination)
+				continue;
+			}
+			
+			//check if ant is still alive (walked to its destination)
+			if (!sortedAnts.contains(ant.getNextPosition())) {
+				it.remove();//ant is dead (no ant at its destination)
+			} else {
+				ant.setPosition(ant.getNextPosition());//now ant position = ant nextPosition
+				sortedAnts.remove(ant.getPosition());//remove ant position from list
+			}
+		}
+		
+		//add new ants to myAntList
+		for (Iterator<Tile> it = sortedAnts.iterator();it.hasNext();){
+			Tile newAntPosition = it.next();
+			myAntList.add(new Ant(newAntPosition));
+		}
 		
 		
+			
 	}
+
 
 	/**
 	 * 
-	 * @return the position of all ants in myAntList.
+	 * @return the positions of all ants in myAntList.
 	 */
 	private List<Tile> getAntPositions() {
 		
-		List<Tile> antPositions = new ArrayList<Tile>();//TODO
-		return null;
+		List<Tile> antPositions = new ArrayList<Tile>();
+		
+		for (Ant ant : myAntList) {
+			antPositions.add(ant.getPosition());
+		}
+		return antPositions;
+	}
+	
+	/**
+	 * 
+	 * @return the destinations of all ants in myAntList.
+	 */
+	private List<Tile> getAntDestinations() {
+		
+		List<Tile> antDestinations = new ArrayList<Tile>();
+		
+		for (Ant ant : myAntList) {
+			antDestinations.add(ant.getDestination());
+		}
+		return antDestinations;
 	}
 
 	/**
@@ -188,13 +307,43 @@ public class GebiBot extends Bot {
 		Ants ants = getAnts();
 		// Track all moves, prevent collisions
 		Tile newLoc = ants.getTile(antLoc, direction);
-		if (ants.getIlk(newLoc).isUnoccupied() && !orders.containsKey(newLoc)) {
+		if (ants.getIlk(newLoc).isUnoccupied() && !orders.containsKey(newLoc)
+				&& !forbiddenPositions.contains(newLoc)) {
 			ants.issueOrder(antLoc, direction);
-			orders.put(newLoc, antLoc);
+			System.out.println("orders-keys: " + orders.keySet() + " || newLoc: " + newLoc);
+			orders.put(newLoc, antLoc);//TODO remove old version
+			
+			//set new next position for ant
+			Ant ant = getAntByLocation(antLoc);
+			if (ant != null) {
+				ant.setNextPosition(newLoc);
+			}
+			
 			return true;
 		} else {
 			return false;
 		}
+	}
+
+	/**
+	 * 
+	 * @param antLoc
+	 * @return ant at given position. null if no ant at position.
+	 */
+	private Ant getAntByLocation(Tile antLoc) {
+		Ant ant = null;
+		for (Iterator<Ant> it = myAntList.iterator(); it.hasNext();) {
+			Ant a = it.next();
+			if (a.getPosition().equals(antLoc)) {
+				ant = a;
+				break;
+			}
+		}
+		
+		if (ant == null) {
+			System.err.println("no ant found for position");
+		}
+		return ant;
 	}
 
 	private boolean doMoveLocation(Tile antLoc, Tile destLoc) {
